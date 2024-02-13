f9804e26eb296d5e7f0388f830ae037250871e8c
Joel Uckelman <uckelman@nomic.net>
2024-02-13 15:08:47 +0000

    Added relative time formatting function.

--- site/gl/js/common.js
+++ site/gl/js/common.js
@@ -1,3 +1,43 @@
+function intlFormatDistance(rtf, date, base) {
+  // https://www.builder.io/blog/relative-time
+
+  // Get the amount of seconds between the given date and now
+  const dsec = Math.round((date.getTime() - base.getTime()) / 1000);
+
+  // Array reprsenting one minute, hour, day, week, month, year in seconds
+  const cutoffs = [
+    60,
+    3600,
+    86400,
+    86400 * 7,
+    86400 * 30,
+    86400 * 365,
+    Infinity
+  ];
+
+  // Array equivalent to the above but in the string representation of the units
+  const units = [
+    "second",
+    "minute",
+    "hour",
+    "day",
+    "week",
+    "month",
+    "year"
+  ];
+
+  // Grab the ideal cutoff unit
+  const unitIndex = cutoffs.findIndex(cutoff => cutoff > Math.abs(dsec));
+
+  // Get the divisor to divide from the seconds. E.g., if our unit is "day"
+  // our divisor is one day in seconds, so we can divide our seconds by
+  // this to get the # of days
+  const divisor = unitIndex ? cutoffs[unitIndex - 1] : 1;
+
+  // Do the formatting
+  return rtf.format(Math.floor(dsec / divisor), units[unitIndex]);
+}
+
 function replaceWithErrorBox(section_to_replace, msg) {
   const e_error = document.createElement('div');
   e_error.classList.add(
