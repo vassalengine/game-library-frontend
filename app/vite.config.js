35d57a7e1e6e1c9d01d508c24b25069d7ef45e6e
Joel Uckelman <uckelman@nomic.net>
2024-05-30 21:49:44 +0100

    Produce source maps for debugging.

--- app/vite.config.js
+++ app/vite.config.js
@@ -5,6 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   build: {
+    sourcemap: true,
     rollupOptions: {
       input: {
         projects: './projects.html',
