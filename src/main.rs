a53b1c71b04ba938224bd8042859b20e1702bd53
Joel Uckelman <uckelman@nomic.net>
2024-07-14 13:41:39 +0100

    Removed cruft.

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
-use askama::Template;
 use axum::{
     Router, serve,
-    extract::Path,
     http::StatusCode,
-    response::{Html, IntoResponse, Response},
-    routing::get
+    response::{IntoResponse, Response},
 };
-use axum_extra::extract::cookie::CookieJar;
 use const_format::formatcp;
 use std::net::SocketAddr;
 use tokio::net::TcpListener;
@@ -15,23 +11,11 @@ use tower_http::{
     services::{ServeDir, ServeFile}
 };
 
-const DISCOURSE_URL: &str = "https://forum.vassalengine.org";
-
 const GL_BASE: &str = "/gl";
 //const GL_BASE: &str = "/test/gl";
 
-const UMS_URL: &str = "http://localhost:4000/api/v1";
-
 const SITE_DIR: &str = "app/dist";
 
-const API_URL: &str = "http://localhost:3000/api/v1";
-//const API_URL: &str = "https://vassalengine.org/test/gls/api/v1";
-const YEAR: &str = "2024";
-const CURRENT_VERSION: &str = "3.7.12";
-const NEWS_LINK: &str = "https://forum.vassalengine.org/t/vassal-3-7-12-released/79548";
-
-// TODO: sanitize strings going into HTML
-// TODO: should empty hrefs be # instead? something else?
 // TODO: look into tracing
 
 enum AppError {
@@ -53,147 +37,6 @@ impl IntoResponse for AppError {
         (code, body).into_response()
     }
 }
-
-struct UserInfo {
-    username: String,
-    name: String,
-    avatar_url: String
-}
-
-#[derive(Template)]
-#[template(path = "projects.html")]
-struct ProjectsTemplate {
-    api_url: String,
-    year: String,
-    current_version: String,
-    news_link: String,
-    user_info: Option<UserInfo>,
-    returnto: String
-}
-
-#[derive(Template)]
-#[template(path = "project.html")]
-struct ProjectTemplate {
-    api_url: String,
-    year: String,
-    current_version: String,
-    news_link: String,
-    user_info: Option<UserInfo>,
-    returnto: String
-}
-
-struct HtmlTemplate<T>(T);
-
-impl<T> IntoResponse for HtmlTemplate<T>
-where
-    T: Template
-{
-    fn into_response(self) -> Response {
-        match self.0.render() {
-            Ok(html) => Html(html).into_response(),
-            Err(err) => (
-                StatusCode::INTERNAL_SERVER_ERROR,
-                format!("Failed to render template. Error: {err}"),
-            ).into_response()
-        }
-    }
-}
-
-async fn setup_user_info(
-    here: &str,
-    jar: CookieJar
-) -> Result<(String, Option<UserInfo>), AppError>
-{
-    let username: Option<String> = jar.get("username")
-        .map(|cookie| cookie.value().to_owned());
-
-    let enc_real_returnto = urlencoding::encode(here);
-
-    match username {
-        Some(username) => {
-            let name = jar.get("name")
-                .map(|cookie| cookie.value().to_owned())
-                .unwrap_or_else(|| username.clone());
-
-            let avatar_url = format!("{UMS_URL}/users/{username}/avatar/48");
-
-            let sso_returnto = format!(
-                "{UMS_URL}/sso/completeLogout?returnto={}",
-                enc_real_returnto
-            );
-
-            let enc_sso_returnto = urlencoding::encode(&sso_returnto);
-
-            Ok(
-                (
-                    enc_sso_returnto.into(),
-                    Some(
-                        UserInfo {
-                            username,
-                            name,
-                            avatar_url
-                        }
-                    )
-                )
-            )
-        },
-        _ => {
-            let sso_returnto = format!(
-                "{UMS_URL}/sso/completeLogin?returnto={}",
-                enc_real_returnto
-            );
-
-            let enc_sso_returnto = urlencoding::encode(&sso_returnto);
-
-            Ok((enc_sso_returnto.into(), None))
-        }
-    }
-}
-
-/*
-async fn handle_projects(
-    jar: CookieJar
-) -> Result<HtmlTemplate<ProjectsTemplate>, AppError> {
-    let here = format!("{GL_BASE}/projects");
-    let (returnto, user_info) = setup_user_info(&here, jar).await?;
-
-    Ok(
-        HtmlTemplate(
-            ProjectsTemplate {
-                api_url: API_URL.into(),
-                year: YEAR.into(),
-                current_version: CURRENT_VERSION.into(),
-                news_link: NEWS_LINK.into(),
-                user_info,
-                returnto
-            }
-        )
-    )
-}
-*/
-
-/*
-async fn handle_project(
-    Path(proj): Path<String>,
-    jar: CookieJar
-) -> Result<HtmlTemplate<ProjectTemplate>, AppError> {
-    let here = format!("{GL_BASE}/projects/{proj}");
-    let (returnto, user_info) = setup_user_info(&here, jar).await?;
-
-    Ok(
-        HtmlTemplate(
-            ProjectTemplate {
-                api_url: API_URL.into(),
-                year: YEAR.into(),
-                current_version: CURRENT_VERSION.into(),
-                news_link: NEWS_LINK.into(),
-                user_info,
-                returnto
-            }
-        )
-    )
-}
-*/
 
 #[tokio::main]
 async fn main() {
