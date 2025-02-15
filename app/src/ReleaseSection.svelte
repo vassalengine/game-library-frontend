996e6f866ed6e83c9c09befda5aaa6ea64919b1b
Joel Uckelman <uckelman@nomic.net>
2025-02-15 14:04:16 +0000

    Sort priority is vmod, vext, vsav, anything else.

--- app/src/ReleaseSection.svelte
+++ app/src/ReleaseSection.svelte
@@ -5,6 +5,29 @@
 
   export let release;
   export let current = false;
+
+  function extPriority(filename) {
+    if (filename.endsWith('.vmod')) {
+      return 0;
+    }
+    else if (filename.endsWith('.vext')) {
+      return 1;
+    }
+    else if (filename.endsWith('.vsav')) {
+      return 2;
+    }
+    else {
+      return 3;
+    }
+  }
+
+  function sortFiles(files) {
+    files.sort((a, b) => {
+      return (extPriority(a.filename) - extPriority(b.filename)) ||
+        a.filename.localeCompare(b.filename);
+    });
+    return files;
+  }
 </script>
 
 <style>
@@ -22,7 +45,7 @@
 <div class="badge rounded-pill fs-5" class:current_release={current} class:release={!current}>{release.version}</div>
 {#if release.files.length > 0}
 <ul class="list-unstyled">
-  {#each release.files as file}
+  {#each sortFiles(release.files) as file}
   <li class="mb-2">
     <FileSection {file} {ums_url} />
   </li>
