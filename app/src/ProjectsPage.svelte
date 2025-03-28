1138e2d95d194dbfad416927d4b86a7c782ac763
Joel Uckelman <uckelman@nomic.net>
2025-03-28 11:01:18 +0000

    Use formdata event for query cleanup.

--- app/src/ProjectsPage.svelte
+++ app/src/ProjectsPage.svelte
@@ -40,11 +40,8 @@
     return s.split(/\s+/).filter(s => s.length > 0).join(' ');
   }
 
-  function submitSearch(event) {
-    const fdata = new FormData(event.target);
-
-    console.log(fdata);
-    fdata.set('q', normalizeWhitespace(fdata.get('q')));
+  function cleanupSearch(event) {
+    event.formData.set('q', normalizeWhitespace(event.formData.get('q')));
   }
 
   function loadProjects(url) {
@@ -114,7 +111,7 @@
 <main class="container px-5 mb-5">
 
 <nav class="d-flex flex-wrap align-items-center my-3">
-  <form class="mx-md-2 my-1 flex-grow-1 order-md-1" action="{base_url}/projects" on:submit={submitSearch}>
+  <form class="mx-md-2 my-1 flex-grow-1 order-md-1" action="{base_url}/projects" on:formdata={cleanupSearch}>
     <input class="form-control" type="search" name="q" placeholder="Search..." value={query} required>
   </form>
   <div class="w-100 d-md-none"></div>
