f86af2344c6ae812c19e847795ea248fba9fc1d2
Joel Uckelman <uckelman@nomic.net>
2025-07-10 11:10:18 +0100

    Fixed typo.

--- app/src/FlagsPage.svelte
+++ app/src/FlagsPage.svelte
@@ -125,7 +125,7 @@ tr:nth-child(even) {
       <tr>
         <td><a href="{base_url}/projects/{flag.project}">{flag.project}</a></td>
         <td>
-          <a href="{discourse_url}/new-message?username={flag.flagged_by}&title=Flag for project {flag.project}&body=> {replyBody(flag)}"><svg class="svg-icon"><use xlink:href="#reply"></use></svg></a>
+          <a href="{discourse_url}/new-message?username={flag.flagged_by}&title=Flag for project {flag.project}&body={replyBody(flag)}"><svg class="svg-icon"><use xlink:href="#reply"></use></svg></a>
           <UserChip {ums_url} username={flag.flagged_by} size=24 />
         </td>
         <td>{flag.flag}</td>
