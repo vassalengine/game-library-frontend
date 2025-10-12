e290719f465d72522c03075480aba269960ece7d
Joel Uckelman <uckelman@nomic.net>
2025-10-12 22:54:08 +0000

    Correctly set the base path fror the root page.

--- app/src/root.js
+++ app/src/root.js
@@ -2,7 +2,7 @@ import RootPage from './RootPage.svelte';
 import { CONFIG } from './lib/setup.js';
 import { mount } from "svelte";
 
-const base_url = window.location.pathname;
+const base_url = window.location.origin + window.location.pathname;
 
 const app = mount(RootPage, {
   target: document.body,
